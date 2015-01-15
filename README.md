86f6fded3c595fa89da2bf7bcb5566b9e228b722
1element <1element@users.noreply.github.com>
2015-01-15 20:28:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+﻿# Amazon Price Watch
+
+Simple script to watch Amazon product prices and send a notification when the price drops below a desired threshold. Useful to watch out for "Warehouse Deals" or used items that are only available for a short time.
+
+Script written in [Node.js](http://nodejs.org/). Uses the [pushover API](https://pushover.net/api/) to send notifications.
+
+
+## Installation
+
+Beside [Node.js](http://nodejs.org/download/) you need the following modules: [Request](https://github.com/request/request), [Cheerio](https://github.com/cheeriojs/cheerio) and [Pushover-Notifications](https://github.com/qbit/node-pushover).
+
+To install the required dependencies execute:
+
+```shell
+npm install request cheerio pushover-notifications
+```
+
+## Configuration
+
+Configuration is done in ```config.json```:
+
+```js
+{
+  "pushover": {
+    "user": "XXX",
+    "token": "XXX"
+  },
+  "global": {
+    "currency": "EUR",
+    "urlPrefix": "http://www.amazon.de/gp/offer-listing/",
+    "urlSuffix": "/ref=olp_tab_all"
+  },
+  "products": [
+    { "name": "Name of product",
+      "productId": "B00CKPCJNO",
+      "priceThreshold": "28.00"
+    },
+    { "name": "Name of product #2",
+      "productId": "B000PKFFNW",
+      "priceThreshold": "90.00"
+    }
+  ]
+}
+```
+
+Change ```pushover.user``` and ```pushover.token``` to your Pushover user and application token.
+
+Depending on your locale you need to change ```global.currency``` and the Amazon domain in ```global.urlPrefix```.
+
+If you only want to monitor new products change the value of ```global.urlSuffix``` to ```/ref=olp_tab_new?condition=new```. To track only used products change it to ```/ref=olp_tab_used?condition=used```.
+
+
+## Usage
+
+Create a crontab entry to execute ```node app.js``` once in a while.
---
+++ app.js
@@ -0,0 +1,62 @@
+﻿/**
+ * Amazon price watch. 
+ * Monitor product prices and send an alert when it drops below a desired threshold.
+ *
+ * https://github.com/1element/product-price-watch
+ */
+var request = require('request');
+var cheerio = require('cheerio');
+var push = require('pushover-notifications');
+var config = require('./config.json');
+
+var requestHandler = function(productName, priceThreshold) {
+  return function(error, response, html) {
+
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+
+      var prices = $('.a-color-price');
+      prices.each(function(i, elem) {
+        var price = $(this).text();
+        price = price.replace(config.global.currency, ''); // remove currency sign
+        price = price.replace(/,/, ".");
+        price = price.trim(); 
+        var plainPrice = parseFloat(price);
+        if (plainPrice < priceThreshold) {
+          console.log('Lower price found for ' + productName + ': ' + plainPrice + '. Threshold was: ' + priceThreshold);
+          sendPush(productName, plainPrice, priceThreshold);
+          return false; 
+        }
+      });
+    }
+
+  };
+};
+
+var sendPush = function(productName, price, priceThreshold) { 
+  var pushover = new push({
+    user: config.pushover.user,
+    token: config.pushover.token 
+  });
+
+  var message = {
+    message: 'Lower price found for ' + productName  + '. Lowest price right now: ' + price + '. Threshold was: ' + priceThreshold,
+    title: 'Price Alert'    
+  };
+
+  pushover.send(message, function(err, result) {
+    if (err) {
+      throw err;  
+    }
+    console.log('Pushover notification was sent.');
+  });
+};
+
+for (var product in config.products) {
+  var productName = config.products[product].name;
+  var productId = config.products[product].productId;
+  var productPriceThreshold = config.products[product].priceThreshold;
+
+  var url = config.global.urlPrefix + productId + config.global.urlSuffix; 
+  request(url, requestHandler(productName, productPriceThreshold));
+}
